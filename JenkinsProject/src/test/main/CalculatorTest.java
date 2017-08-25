d99e30ab1b8db062adbe09561fdb79d2ec5aaa89
Arnab <sengarnab@gmail.com>
2017-08-25 12:26:38 +0530

    changes made

--- JenkinsProject/src/test/main/CalculatorTest.java
+++ JenkinsProject/src/test/main/CalculatorTest.java
@@ -3,8 +3,8 @@ package test.main;
 public class CalculatorTest {
 	
 	
-	public static void main(String args){
-		System.out.println("Hello world!!!");
+	public static void main(String[] args){
+		System.out.println("Hello world!!!")
 	}
 
 }
